85967b892ec3cc4c612bb953c101198598b8a31c
Mario Cardoso <cardosomarioar@gmail.com>
2018-01-11 08:49:00 +0000

    use arrow functions instead

--- actions/index.js
+++ actions/index.js
@@ -2,24 +2,18 @@ export const RECEIVE_DECKS = 'RECEIVE_DECKS'
 export const ADD_DECK = 'ADD_DECK'
 export const ADD_CARD = 'ADD_CARD'
 
-export function receiveDecks (decks) {
-  return {
-    type: RECEIVE_DECKS,
-    decks,
-  }
-}
+export const receiveDecks = (decks) => ({
+  type: RECEIVE_DECKS,
+  decks
+})
 
-export function addDeck (deck) {
-  return {
-    type: ADD_DECK,
-    deck,
-  }
-}
+export const addDeck = (deck) => ({
+  type: ADD_DECK,
+  deck
+})
 
-export function addCard (deckId, card) {
-  return {
-    type: ADD_CARD,
-    deckId,
-    card,
-  }
-}
+export const addCard = (deckId, card) => ({
+  type: ADD_CARD,
+  deckId,
+  card,
+})
--- utils/api.js
+++ utils/api.js
@@ -1,23 +1,23 @@
 import { AsyncStorage } from 'react-native'
 import { formatDecksResults, DECKS_STORAGE_KEY } from './_deck'
 
-export function fetchDecks () {
-  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+export const fetchDecks = () => (
+  AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then(formatDecksResults)
-}
+)
 
-export function submitDeck ({ key, deck }) {
-  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+export const submitDeck = ({ key, deck })  => (
+  AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
     [key]: deck
   }))
-}
+)
 
-export function submitCard ({ deckId, card }) {
-  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+export const submitCard = ({ deckId, card }) => (
+  AsyncStorage.getItem(DECKS_STORAGE_KEY)
     .then((results) => {
       const data = JSON.parse(results)
       data[deckId].questions.push(card)
       AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
       return card
     })
-}
+)
